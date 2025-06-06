0fd8d01577975b030ad33f7509de3ac556ce9ed8
Mariam Lawal <160773992+TVY0R@users.noreply.github.com>
2025-06-06 14:51:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Spot",
-  build: {
-    outDir: 'build',
-  },
-});
+  base: process.env.VITE_BASE_PATH || "/Spot"
+})
